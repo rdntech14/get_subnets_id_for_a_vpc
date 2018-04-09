d5d791ec2932d9d193bad4d46628dc8795c64dae
RDN Tech <rndtech14@gmail.com>
2018-04-09 19:17:14 -0400

    second commit

--- main.tf
+++ main.tf
@@ -2,19 +2,25 @@ provider "aws" {
   region = "${var.AWS_REGION}"
 }
 
-data "aws_subnet_ids" "subnets" {
+# Below two data soruce blocks are mandatory
+data "aws_subnet_ids" "subnets1" {
   vpc_id = "${var.vpc_id}"
 }
 
 data "aws_subnet" "subnets" {
-  count = "${length(data.aws_subnet_ids.subnets.ids)}"
-  id    = "${data.aws_subnet_ids.subnets.ids[count.index]}"
-}
+  count = "${length(data.aws_subnet_ids.subnets1.ids)}"
 
-output "subnets_count" {
-  value = "${length(data.aws_subnet_ids.subnets.ids)}"
+  id = "${data.aws_subnet_ids.subnets1.ids[count.index]}"
 }
 
 output "subnets_id" {
   value = ["${data.aws_subnet.subnets.*.id}"]
+}
+
+output "subnets_id_1" {
+  value = ["${data.aws_subnet.subnets.0.id}"]
+}
+
+output "subnets_count" {
+  value = "${length(data.aws_subnet_ids.subnets1.ids)}"
 }
